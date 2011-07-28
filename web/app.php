eca1ec28261375cf5ffba25cba681c9b9196e360
Anton Babenko <anton@antonbabenko.com>
2011-07-28 16:27:22 +0200

    Some changes

--- web/app.php
+++ web/app.php
@@ -1,12 +1,17 @@
 <?php
 
+require_once __DIR__.'/access_config.php';
+
+if (!user_has_access()) die();
+
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('dev', false);
+//$kernel = new AppCache(new AppKernel('prod', false));
+$kernel = new AppKernel(SF_ENVIRONMENT, SF_DEBUG);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 $kernel->handle(Request::createFromGlobals())->send();
