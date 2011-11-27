42b73531772730d573bac88a35f5f57a543210c0
Anton Babenko <anton@antonbabenko.com>
2011-11-27 16:00:44 +0100

    Moved stuff to use MongoDB instead of Redis

--- src/Imagepush/ImagepushBundle/Controller/ImportController.php
+++ src/Imagepush/ImagepushBundle/Controller/ImportController.php
@@ -7,105 +7,233 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
 use Imagepush\ImagepushBundle\Entity\Image as RedisImage;
 use Imagepush\ImagepushBundle\Document\Image;
+use Imagepush\ImagepushBundle\Document\Tag;
+use Imagepush\ImagepushBundle\Document\LatestTag;
 
 class ImportController extends Controller
 {
 
   /**
    * @Route("/", name="importIndex")
-   * @Template()
+   * @Template("::base.html.twig")
    */
   public function indexAction()
   {
-    $result = $this->importImages();
+    $result = $this->importTags();
+    //$result = $this->importLatestTags();
+    //$result = $this->importImages();
     
-    return new Response("OK");
+    echo "All done :)";
+
+    return array();
   }
-  
-  private function importImages() {
+
+  private function importTags()
+  {
+
+    $dm = $this->get('doctrine.odm.mongodb.document_manager');
+    $redis = $this->get('snc_redis.default_client');
+    $i = 0;
+
+    $dm->getDocumentCollection("ImagepushBundle:Tag")->drop();
+
+    $tags = $this->get('imagepush.tags.manager')->getAllHumanTagsWithIds();
     
+    //\D::dump($tags);
+    if (count($tags))
+    {
+      foreach ($tags as $legacyKey => $tag) {
+        $text = $tag;
+        
+        // replace
+        if (strstr($legacyKey, "_replace")) {
+          //\D::dump($redis->get(str_replace("_replace", "",$legacyKey)));
+          //\D::dump($redis->get($text));
+          $text = $redis->get($text);
+        }
+        
+        // replace newline
+        $text = str_replace("\n", " ", $text);
+        
+        $new = new Tag();
+        $new->setLegacyKey($legacyKey);
+        $new->setText($text);
+        
+        //$count = $redis->zscore("tag_usage", $legacyKey);
+        $availableCount = $redis->zcard("image_list:". $legacyKey);
+        $upcomingCount = $redis->zcard("upcoming_image_list:". $legacyKey);
+        
+        $new->setUsedInAvailable((int)$availableCount);
+        $new->setUsedInUpcoming((int)$upcomingCount);
+
+        $dm->persist($new);
+
+        if (++$i % 100 == 0)
+        {
+          $dm->flush();
+          $dm->clear();
+        }
+      }
+
+      $dm->flush();
+      $dm->clear();
+    }
+  }
+
+  private function importImages($limit = 9999999)
+  {
+
     $dm = $this->get('doctrine.odm.mongodb.document_manager');
     $i = 0;
-    
-    // current
-    $images = $this->get('imagepush.images.manager')->getImages("current", 5);
 
-    \D::dump($images);
+    $dm->getDocumentCollection("ImagepushBundle:Image")->drop();
+
+    $allImages = array(
+      "current" => $this->get('imagepush.images.manager')->getImages("current", $limit),
+      "upcoming" => $this->get('imagepush.images.manager')->getImages("upcoming", $limit)
+    );
+
+    //\D::dump($allImages);
+
+    foreach ($allImages as $imageGroup => $images) {
+
+      if (count($images))
+      {
+        foreach ($images as $image) {
+          $new = new Image();
+          $new->setIsAvailable($imageGroup == "current");
+          $new->setId($image["id"]);
+          $new->setLink($image["link"]);
+          $new->setTitle($image["title"]);
+          $new->setSlug($image["slug"]);
+          if (!empty($image["timestamp"]))
+          {
+            $new->setTimestamp((int) $image["timestamp"]);
+          }
+          if (!empty($image["m_file"]))
+          {
+            $new->setFile($image["m_file"]);
+          } elseif (!empty($image["file"]))
+          {
+            $new->setFile($image["file"]);
+          }
+          if (!empty($image["source_type"]))
+          {
+            $new->setSourceType($image["source_type"]);
+          }
+          if (!empty($image["source_tags"]))
+          {
+            $new->setSourceTags(json_decode($image["source_tags"], true));
+          }
+          if (!empty($image["m_width"]))
+          {
+            $new->setMWidth($image["m_width"]);
+          }
+          if (!empty($image["m_height"]))
+          {
+            $new->setMHeight($image["m_height"]);
+          }
+          if (!empty($image["thumb_width"]))
+          {
+            $new->setTWidth($image["thumb_width"]);
+          }
+          if (!empty($image["thumb_height"]))
+          {
+            $new->setTHeight($image["thumb_height"]);
+          }
+          if (!empty($image["a_width"]))
+          {
+            $new->setAWidth($image["a_width"]);
+          }
+          if (!empty($image["a_height"]))
+          {
+            $new->setAHeight($image["a_height"]);
+          }
+          if (!empty($image["_tags"]))
+          {
+
+            $tags = array();
+            
+            foreach ($image["_tags"] as $oneTag) {
+              $tag = $dm->getRepository("ImagepushBundle:Tag")->findOneBy(array("text" => $oneTag));
+              if ($tag)
+              {
+                $tags[] = $oneTag;
+                $new->addTagsRef($tag);
+                $tag->addImagesRef($new);
+                $dm->persist($tag);
+              } else
+              {
+                \D::dump($oneTag);
+              }
+            }
+            
+            //if (count($tags)) {
+              $new->setTags($tags);
+            //}
+
+          }
+
+          $dm->persist($new);
+
+          if (++$i % 100 == 0)
+          {
+            $dm->flush();
+            $dm->clear();
+          }
+        }
+
+        $dm->flush();
+        $dm->clear();
+      }
+    }
+  }
+  
+  
+  private function importLatestTags()
+  {
+
+    $dm = $this->get('doctrine.odm.mongodb.document_manager');
+    $redis = $this->get('snc_redis.default_client');
     
-    if (count($images))
+    $i = 0;
+
+    $dm->getDocumentCollection("ImagepushBundle:LatestTag")->drop();
+
+    // latest trend: get last 300 from the list and count how often they used
+    $latestTags = $redis->lrange("latest_tags", -300, -1); // put 0 instead of -300 for complete import
+
+    //\D::dump($latestTags);
+    //die();
+    if (count($latestTags))
     {
-      foreach ($images as $image) {
-        $new = new Image();
-        $new->setImageId($image["id"]);
-        $new->setLink($image["link"]);
-        $new->setTitle($image["title"]);
-        $new->setSlug($image["slug"]);
-        if (!empty($image["timestamp"])) {
-          $new->setTimestamp((int)$image["timestamp"]);
-        }
-        if (!empty($image["m_file"]))
-        {
-          $new->setFile($image["m_file"]);
-        } elseif (!empty($image["file"]))
-        {
-          $new->setFile($image["file"]);
-        }
-        if (!empty($image["source_type"]))
-        {
-          $new->setSourceType($image["source_type"]);
-        }
-        if (!empty($image["source_tags"]))
-        {
-          $new->setSourceTags(json_decode($image["source_tags"], true));
-        }
-        if (!empty($image["m_width"]))
-        {
-          $new->setMWidth($image["m_width"]);
-        }
-        if (!empty($image["m_height"]))
-        {
-          $new->setMHeight($image["m_height"]);
-        }
-        if (!empty($image["t_width"]))
-        {
-          $new->setTWidth($image["t_width"]);
-        }
-        if (!empty($image["t_height"]))
-        {
-          $new->setTHeight($image["t_height"]);
-        }
-        if (!empty($image["a_width"]))
-        {
-          $new->setAWidth($image["a_width"]);
-        }
-        if (!empty($image["a_height"]))
-        {
-          $new->setAHeight($image["a_height"]);
-        }
-        if (!empty($image["_tags"]))
-        {
-          $new->setTags($image["_tags"]);
-        }
+      foreach ($latestTags as $latestTag) {
+        $new = new LatestTag();
+        $new->setTimestamp(time());
         
-        $dm->persist($new);
-        
+        $tag = $dm->getRepository("ImagepushBundle:Tag")->findOneBy(array("legacyKey" => $latestTag));
+        //\D::dump($tag);
+        if ($tag)
+        {
+          $new->setTag($tag);
+          $dm->persist($new);
+        } else
+        {
+          //\D::dump($latestTag);
+        }
         
         if (++$i % 100 == 0)
         {
           $dm->flush();
           $dm->clear();
         }
-        
       }
-      
+
       $dm->flush();
       $dm->clear();
-    
     }
-    
   }
 
 }
--- src/Imagepush/ImagepushBundle/Document/ImageRepository.php
+++ src/Imagepush/ImagepushBundle/Document/ImageRepository.php
@@ -6,167 +6,101 @@ use Doctrine\ODM\MongoDB\DocumentRepository;
 
 class ImageRepository extends DocumentRepository
 {
-    public function findAllOrderedByName()
-    {
-        return $this->createQueryBuilder()
-            ->sort('name', 'ASC')
-            ->getQuery()
-            ->execute();
-    }
 
+  public function findAllOrderedByName()
+  {
+    return $this->createQueryBuilder()
+        ->sort('name', 'ASC')
+        ->getQuery()
+        ->execute();
+  }
 
   /*
    * @return array()
    */
-  public function findImages($type, $limit = 20, $params = array()) {
-    
-    if (!in_array($type, array("current", "upcoming"))) {
+
+  public function findImages($type, $limit = 20, $params = array())
+  {
+
+    if (!in_array($type, array("current", "upcoming")))
+    {
       throw new \ErrorException(sprintf("Incorrect image type: %s", $type));
     }
-    
-    if (!is_array($params)) {
+
+    if (!is_array($params))
+    {
       throw new \ErrorException(sprintf("Params should be an array, but %s given", gettype($params)));
     }
-    
-    if ($type == "current") {
-      $fieldName = "image_list";
-      $filterDislikedImages = false;
-    } else {
-      $fieldName = "upcoming_image_list";
-      $filterDislikedImages = true;
-    }
-    
+
+    //\D::dump($params);
     extract($params);
 
-    /*if (isset($tag)) {
-      $fieldName .= ':'.$this->tagsManager->getTagKey($tag);
-    }*/
+    $query = $this->createQueryBuilder();
 
-    //$image_keys = $this->redis->zrevrangebyscore($fieldName, "+inf", "-inf", array("LIMIT" => array(0, $limit)));
-    //\D::dump($image_keys);
-    
-    return $this->createQueryBuilder()
-          //->sort('name', 'ASC')
-          ->limit($limit)
-          ->getQuery()
-          ->execute();
-    
+    // Tag or tags
+    if (isset($tag))
+    {
+      $query = $query
+          ->field('tags')->in((array) $tag);
+    }
+
+    $query = $query
+      ->field('isAvailable')->equals($type == "current")
+      ->sort('timestamp', 'DESC')
+      ->limit($limit)
+      ->getQuery();
+
+    return $query->toArray();
   }
-  
+
   /*
    * @return array()|false
    */
-  public function getOneImage($id) {
+
+  public function getOneImage($id)
+  {
 
     $key = $this->getImageKey($id);
-    
-    if ($this->redis->sismember('available_images', $key)) {
+
+    if ($this->redis->sismember('available_images', $key))
+    {
       $image = $this->redis->hgetall($key);
       return $this->normalizeImage($image);
-    } else {
+    } else
+    {
       return false;
     }
-
   }
 
   /*
    * @return array()|false
    */
-  public function getImageByKey($key) {
 
-    return $this->redis->hgetall($key);
+  public function getOneImageRelatedToTimestamp($direction, $timestamp)
+  {
 
-  }
+    if (!$timestamp || !in_array($direction, array("next", "prev")))
+      return false;
 
-  /*
-   * @return array()|false
-   */
-  public function getOneImageRelatedToTimestamp($direction, $timestamp) {
+    $query = $this->createQueryBuilder();
 
-    if (!$timestamp || !in_array($direction, array("next", "prev"))) return false;
-    
-    if ($direction == "next") {
-      $key = $this->redis->zrangebyscore('image_list', $timestamp, "+inf", array("LIMIT" => array(1, 1)));
-    } else {
-      $key = $this->redis->zrevrangebyscore('image_list', $timestamp, "-inf", array("LIMIT" => array(1, 1)));
-    }
-    
-    if (!empty($key[0]))
+    if ($direction == "next")
     {
-      $image = $this->redis->hgetall($key[0]);
+      $query = $query
+        ->field('timestamp')->gt($timestamp)
+        ->sort('timestamp', 'ASC');
+    } else
+    {
+      $query = $query
+        ->field('timestamp')->lt($timestamp)
+        ->sort('timestamp', 'DESC');
     }
 
-    return isset($image) ? $image : false;
+    $query = $query
+      ->field('isAvailable')->equals(true)
+      ->getQuery();
 
+    return $query->getSingleResult();
   }
 
-  /*
-   * Filter out images current user disliked
-   * @return array()
-   */
-  public function filterDislikedImages($image_keys = array()) {
-
-    $user = $_SERVER["REMOTE_ADDR"];
-    
-    $disliked = $this->redis->smembers("user_dislikes:".$user);
-
-    if (count($disliked)) {
-      $image_keys = array_diff($image_keys, $disliked);
-    }
-
-    return $image_keys;
-
-  }
-  
-  /////
-  public static function getFileUrl($image, $format) {
-
-    if (empty($format)) {
-      throw new \ErrorException(sprintf("Unknown image format '%s'", $format));
-    }
-    
-    $prefix = "/uploads";
-    
-    if (!empty($image["file"])) {
-      
-      $url = $prefix."/".$format."/".$image["file"];
-      
-    } elseif (!empty($image[$format."_file"])) { // old data in db
-      
-      $url = $prefix."/".$format."/".$image[$format."_file"];
-      
-    } elseif ($format == "t" && !empty($image["thumb_file"])) { // folder "thumb" to be renamed to "t"
-      
-      $url = $prefix."/t/".$image["thumb_file"];
-      
-    } else {
-      $url = "";
-    }
-
-    return $url;
-
-  }
-
-  /*
-   * Verify that image has all required fields and define correct url fields
-   */
-  public function normalizeImage($image) {
-    
-    if (!count($image)) return false;
-    
-    $image["_thumb_img"] = $this->getFileUrl($image, "t"); // thumb
-    $image["_main_img"] = $this->getFileUrl($image, "m"); // main
-    $image["_article_img"] = $this->getFileUrl($image, "a"); // article
-    $image["_tags"] = (isset($image["tags"]) && json_decode($image["tags"]) ? $this->tagsManager->getHumanTags(json_decode($image["tags"])) : "");
-    $image["_view_url"] = $this->router->generate('viewImage', array('id' => $image["id"], 'slug' => $image["slug"]));
-    $image["_share_url"] = $this->router->generate('viewImage', array('id' => $image["id"], 'slug' => $image["slug"]), true);
-    $image["_original_host"] = @parse_url($image["link"], PHP_URL_HOST);
-    $image["_date"] = date(DATE_W3C, $image["timestamp"]);
-
-    //\D::dump($image);
-
-    return $image;
-    
-  }
-  
 }
