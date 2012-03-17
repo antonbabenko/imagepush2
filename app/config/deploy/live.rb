be2b6683ff441e1dbc58dbe98f1aeba20780032e
Anton Babenko <anton@antonbabenko.com>
2012-03-17 18:50:21 +0100

    Capifony

---
+++ app/config/deploy/live.rb
@@ -0,0 +1,9 @@
+server 'imagepush.to', :app, :web, :primary => true
+set :deploy_to, "/mnt/www/imagepush"
+
+# Made vendor not shared. It is slower, but more secure.
+set :vendors_mode, "install"
+
+set :branch, "master"
+
+set :shared_children,     [app_path + "/logs", "web/uploads"]
