db9a9f7afe30db4bbda4a6c9b153c242d5f7ddbb
Anton Babenko <anton@antonbabenko.com>
2012-07-09 11:23:04 +0200

    Vote/flag

---
+++ src/Imagepush/ImagepushBundle/Document/VoteRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Imagepush\ImagepushBundle\Document;
+
+use Doctrine\ODM\MongoDB\DocumentRepository;
+use Imagepush\ImagepushBundle\Document\Link;
+
+class VoteRepository extends DocumentRepository
+{
+
+}
