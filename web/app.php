5dee5e43863355554bbf43213250f14391057b82
Anton Babenko <anton@antonbabenko.com>
2013-04-03 22:40:55 +0200

    fixed esi _fragment

--- web/app.php
+++ web/app.php
@@ -2,24 +2,23 @@
 
 umask(0000);
 
+use Symfony\Component\HttpFoundation\Request;
+
 require_once __DIR__.'/access_config.php';
 
 if (!user_has_access()) die();
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/bootstrap_cache.php.cache';
 //require_once __DIR__.'/../app/AppCache.php';
 
-use Symfony\Component\HttpFoundation\Request;
-
-//$kernel = new AppCache(new AppKernel('prod', false));
 $kernel = new AppKernel(SF_ENVIRONMENT, SF_DEBUG);
 $kernel->loadClassCache();
-$request = Request::createFromGlobals();
+//$kernel = new AppCache($kernel);
 
 Request::enableHttpMethodParameterOverride();
 
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
