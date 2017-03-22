bd3665f5a43798cb8374532661d7a50c5f85c75f
Anton Babenko <anton@antonbabenko.com>
2017-03-22 18:50:38 +0100

    Allow dev-eb environment

--- web/access_config.php
+++ web/access_config.php
@@ -2,21 +2,22 @@
 
 function user_has_access()
 {
-  if (preg_match("/^dev/", $_SERVER['HTTP_HOST'])) {
-    define('SF_ENVIRONMENT', 'dev');
-    define('SF_DEBUG', true);
-    $allowed_ips = array('127.0.0.1', '::1');
-  } else {
-    define('SF_ENVIRONMENT', 'prod');
-    define('SF_DEBUG', false);
-    $allowed_ips = array();
-  }
 
-  if ( SF_ENVIRONMENT == "dev" && !in_array(@$_SERVER['REMOTE_ADDR'], $allowed_ips) ) {
-    echo 'You are not allowed to access this site. Ask somone for more information.';
+    if (preg_match("/^dev/", $_SERVER['HTTP_HOST']) && !preg_match("/elasticbeanstalk\.com$/", $_SERVER['HTTP_HOST'])) {
+        define('SF_ENVIRONMENT', 'dev');
+        define('SF_DEBUG', true);
+        $allowed_ips = array('127.0.0.1', '::1');
+    } else {
+        define('SF_ENVIRONMENT', 'prod');
+        define('SF_DEBUG', false);
+        $allowed_ips = array();
+    }
 
-    return false;
-  } else {
-    return true;
-  }
+    if (SF_ENVIRONMENT == "dev" && !in_array(@$_SERVER['REMOTE_ADDR'], $allowed_ips)) {
+        echo 'You are not allowed to access this site. Ask someone for more information.';
+
+        return false;
+    } else {
+        return true;
+    }
 }
